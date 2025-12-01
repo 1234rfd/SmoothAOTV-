416f381d625312843b642543265fa50e1c8463cf
1234rfd <lickmaballsdb@gmail.com>
2025-12-01 12:02:33 -0500

    Add SmoothAOTV mod with initialization handler

---
+++ src/main/java/com/smoothaotv/smoothaotv/SmoothAOTV.java
@@ -0,0 +1,14 @@
+package com.smoothaotv.smoothaotv;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+@Mod(modid = "smoothaotv", name = "Smooth AOTV", version = "1.0", clientSideOnly = true)
+public class SmoothAOTV {
+    
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(new SmoothAOTVHandler());
+    }
+}
