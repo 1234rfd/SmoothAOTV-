334c662f7fedc1843d51171eaea66e753cd0ab71
1234rfd <lickmaballsdb@gmail.com>
2025-12-01 12:26:52 -0500

    Refactor gradlew.bat script for clarity

--- gradlew.bat
+++ gradlew.bat
@@ -1,44 +1,22 @@
 @echo off
-@rem -----------------------------------------------------------------------------
-@rem Gradle startup script for Windows
-@rem -----------------------------------------------------------------------------
+REM ---------------------------------------------------------------------------
+REM Gradle startup script for Windows
+REM ---------------------------------------------------------------------------
+SETLOCAL
+SET DIRNAME=%~dp0
+IF "%DIRNAME%"=="" SET DIRNAME=.
 
-setlocal
-set DIRNAME=%~dp0
-if "%DIRNAME%" == "" set DIRNAME=.
+SET APP_BASE_NAME=%~n0
+SET APP_HOME=%DIRNAME%
 
-set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%
+SET DEFAULT_JVM_OPTS=-Xmx1024m -Xms256m
 
-set DEFAULT_JVM_OPTS=-Xmx1024m -Xms256m
-
-set JAVA_EXE=java.exe
-if defined JAVA_HOME (
-  if exist "%JAVA_HOME%\bin\java.exe" set JAVA_EXE=%JAVA_HOME%\bin\java.exe
+SET JAVA_EXE=java.exe
+IF DEFINED JAVA_HOME (
+    IF EXIST "%JAVA_HOME%\bin\java.exe" SET JAVA_EXE=%JAVA_HOME%\bin\java.exe
 )
 
-:findMaxFD
-call :findMaxFD2 16384
-if errorlevel 1 goto :findMaxFDDone
-call :findMaxFD2 8192
-if errorlevel 1 goto :findMaxFDDone
-call :findMaxFD2 4096
-if errorlevel 1 goto :findMaxFDDone
-set MAX_FD=2048
-goto :findMaxFDDone
+SET CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
-:findMaxFD2
-ulimit -H -n > nul 2>&1 || exit /b 1
-exit /b 0
-
-:findMaxFDDone
-
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
-
-if exist "%JAVA_EXE%" goto executeJava
-echo Could not find java executable.
-exit /b 1
-
-:executeJava
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
-exit /b %ERRORLEVEL%
+EXIT /B %ERRORLEVEL%
