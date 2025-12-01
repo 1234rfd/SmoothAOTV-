7e25cdcd9413dc9ab05b86b19e077f2671808c3c
1234rfd <lickmaballsdb@gmail.com>
2025-12-01 12:07:45 -0500

    Remove unnecessary whitespace in SmoothAOTV.java

--- src/main/java/com/smoothaotv/smoothaotv/SmoothAOTV.java
+++ src/main/java/com/smoothaotv/smoothaotv/SmoothAOTV.java
@@ -6,7 +6,6 @@ import net.minecraftforge.common.MinecraftForge;
 
 @Mod(modid = "smoothaotv", name = "Smooth AOTV", version = "1.0", clientSideOnly = true)
 public class SmoothAOTV {
-    
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(new SmoothAOTVHandler());
